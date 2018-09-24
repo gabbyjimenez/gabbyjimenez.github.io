40a63ff12da0d902d8f01b0035dfa9a199b2f336
Gabby Jimenez <gabbyjimnz@gmail.com>
2018-09-23 20:45:49 -0700

    finished lab 7

--- student2.js
+++ student2.js
@@ -3,9 +3,14 @@ var phonenumber = "858-555-6789";
 var address = "123 Main St";
 
 function getHeader() {
-
+    var headerHTML = "<div> <h1> Welcome to " + companyname  + "</div> </h1>";
+    document.getElementById("headerDiv").innerHTML = headerHTML;
 }
 function getFooter() {
 
-    
+    var headerHTML = "<div>" +companyname + "<br>" + address + "<br>" + phonenumber+ "</div>";
+    document.getElementById("footerDiv").innerHTML = headerHTML;
 }
+
+getHeader();
+getFooter();
